5421d86ded2d012c63c18d8183a3eca21425bae4
GitanshiG <gitanshi.2327cseml10@kiet.edu>
2026-03-26 23:31:18 +0530

    Project Setup And LLM Chatbot Implementation

---
+++ llm.py
@@ -0,0 +1,24 @@
+from config import GROQ_API_KEY
+from groq import Groq
+
+client = Groq(
+    api_key=GROQ_API_KEY,
+)
+
+def generate_response(prompt):
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": prompt,
+            }
+        ],
+        model="llama-3.3-70b-versatile",
+    )
+    
+    return chat_completion.choices[0].message.content
+
+while True:
+    input_prompt = input("Enter your prompt: ")
+    response = generate_response(input_prompt)
+    print("Response: ", response)
